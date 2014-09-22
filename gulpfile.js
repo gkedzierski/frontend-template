0fd3b63d2c0507b05986d4e3b656f5dc6693c03e
gkedzierski <greg@gregkedzierski.com>
2014-09-22 09:48:52 +0200

    Add autoprefixer and minify css to the dev workflow

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,17 @@
 // Require modules
-var gulp        = require('gulp');
-var browserSync = require('browser-sync');
-var reload      = browserSync.reload;
-var sass        = require("gulp-ruby-sass");
-var scsslint    = require('gulp-scss-lint');
-var browserify  = require('gulp-browserify');
-var jshint      = require('gulp-jshint');
-var stylish     = require('jshint-stylish');
-var filter      = require('gulp-filter');
-var cache       = require('gulp-cached');
-var copy        = require('gulp-copy');
+var gulp         = require('gulp');
+var browserSync  = require('browser-sync');
+var reload       = browserSync.reload;
+var sass         = require("gulp-ruby-sass");
+var scsslint     = require('gulp-scss-lint');
+var browserify   = require('gulp-browserify');
+var jshint       = require('gulp-jshint');
+var stylish      = require('jshint-stylish');
+var filter       = require('gulp-filter');
+var cache        = require('gulp-cached');
+var copy         = require('gulp-copy');
+var autoprefixer = require('gulp-autoprefixer');
+var minifyCSS    = require('gulp-minify-css');
 
 // Configuration
 var config = {
@@ -28,7 +30,8 @@ var config = {
 // Create a browser sync server
 gulp.task('browser-sync', function() {
     browserSync({
-        proxy: 'frontend-template.vm.com'
+        proxy : 'frontend-template.vm.com',
+        open  : false
     });
 });
 
@@ -36,6 +39,11 @@ gulp.task('browser-sync', function() {
 gulp.task('sass', function () {
     return gulp.src(config.paths.src.sass + '/main.scss')
         .pipe(sass())
+        .pipe(autoprefixer({
+            browsers : ['> 1%', 'last 3 versions'],
+            cascade  : false
+        }))
+        .pipe(minifyCSS())
         .pipe(gulp.dest(config.paths.dist.css))
         .pipe(filter('**/*.css'))
         .pipe(reload({
